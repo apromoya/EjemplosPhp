acdd7fcb99a1844c6d286b2f9830889b2133d443
apromoya <oskitarlopez9@gmail.com>
2024-07-24 20:47:47 -0500

    Se termino todo la paginacion

--- laod.php
+++ laod.php
@@ -6,6 +6,8 @@
     #Si uno quiere  sea limitar hasta la condicion borra los campos que no deseas que busque 
     $table = 'clientes';
 
+    $id = 'identificacion';
+
     $campo = isset($_POST['campo']) ? $con->real_escape_string($_POST['campo']) : null;
 
     $where = '';
@@ -20,32 +22,84 @@
         $where = substr_replace($where, "", -3);
         $where .= ")";
     }
+    #Limit
+    $limit = isset($_POST['registros']) ? $con->real_escape_string($_POST['registros']) : 10;
+    $pagina = isset($_POST['pagina']) ? $con->real_escape_string($_POST['pagina']) : 0;
 
-    $sql = "SELECT " . implode(", ",$columns) . "
+    if(!$pagina){
+        $inicio = 0;
+        $pagina = 1;
+    } else {
+        $inicio = ($pagina - 1) * $limit;
+    }
+    $sLimit = "LIMIT $inicio, $limit";
+
+    $sql = "SELECT SQL_CALC_FOUND_ROWS " . implode(", ",$columns) . "
     FROM $table
-    $where";
+    $where
+    $sLimit";
     $resultados = $con->query($sql);
     $num_rows = $resultados->num_rows;
+
+    #Consulta para total de friltos
+    $sqlFiltro = "SELECT FOUND_ROWS()";
+    $resFiltro = $con->query($sqlFiltro);
+    $row_Filtro = $resFiltro->fetch_array();
+    $totalFiltro = $row_Filtro[0];
+    #Consulta para total de friltos
+    $sqlTotal = "SELECT count($id) FROM $table ";
+    $resTotal = $con->query($sqlTotal);
+    $row_Total = $resTotal->fetch_array();
+    $totalRegistros = $row_Total[0];
     
-    $html = '';
+    #
+    $output = [];
+    $output['totalRegistros'] = $totalRegistros;
+    $output['totalFiltro'] = $totalFiltro;
+    $output['data'] = '';
+    $output['paginacion'] = '';
 
     if ($num_rows > 0) {
         while($row = $resultados->fetch_assoc()){
-            $html .= '<tr>';
-            $html .= '<td>'.$row['identificacion'].'</td>';
-            $html .= '<td>'.$row['nombres'].'</td>';
-            $html .= '<td>'.$row['apellidos'].'</td>';
-            $html .= '<td>'.$row['sexo'].'</td>';
-            $html .= '<td>'.$row['factura'].'</td>';
-            $html .= '<td>'.$row['calificacion'].'</td>';
-            $html .= '<td> <a href="">Editar</a></td>';
-            $html .= '<td><a href="">Eliminar</a></td>';
-            $html .= '</tr>';
+            $output['data'] .= '<tr>';
+            $output['data'] .= '<td>'.$row['identificacion'].'</td>';
+            $output['data'] .= '<td>'.$row['nombres'].'</td>';
+            $output['data'] .= '<td>'.$row['apellidos'].'</td>';
+            $output['data'] .= '<td>'.$row['sexo'].'</td>';
+            $output['data'] .= '<td>'.$row['factura'].'</td>';
+            $output['data'] .= '<td>'.$row['calificacion'].'</td>';
+            $output['data'] .= '<td> <a href="">Editar</a></td>';
+            $output['data'] .= '<td><a href="">Eliminar</a></td>';
+            $output['data'] .= '</tr>';
         }
     } else {
-        $html .= '<tr>';
-        $html .= '<td colspan="7">Sin resultados</td>';
-        $html .= '</tr>';
+        $output['data'] .= '<tr>';
+        $output['data'] .= '<td colspan="7">Sin resultados</td>';
+        $output['data'] .= '</tr>';
     }
-    echo json_encode($html, JSON_UNESCAPED_UNICODE);
+
+    if($output['totalRegistros']>0){
+        $totalPaginas = ceil($output['totalRegistros'] / $limit);
+        $output['paginacion'] .= '<nav>';
+        $output['paginacion'] .= '<ul class="pagination">';
+        $numeroInicio = 1;
+        if(($pagina - 4) > 1){
+            $numeroInicio = $pagina - 4;
+        }
+        $numeroFin = $numeroInicio + 9;
+        if($numeroFin > $totalPaginas){
+            $numeroFin = $totalPaginas;
+        }
+        for($i=1; $i <= $totalPaginas; $i++){
+            if($pagina == 1){
+            $output['paginacion'] .= '<li class="page-item"><a class="page-link active" href="#">'.$i.'</a></li>';
+            } else {
+            $output['paginacion'] .= '<li class="page-item"><a class="page-link" href="#" onclick="getData('.$i.')">'.$i.'</a></li>';
+
+            }
+        }
+        $output['paginacion'] .= '</ul>';
+        $output['paginacion'] .= '</nav>';
+    }
+    echo json_encode($output, JSON_UNESCAPED_UNICODE);
     ?>
--- paginacion.php
+++ paginacion.php
@@ -11,6 +11,19 @@
     ?>
     <h1>Empleados</h1>
     <a href="index.html">Volver</a>
+    <div>
+        <label for="num_registros">Mostrar: </label>
+    </div>
+    <div>
+        <select name="num_registros" id="num_registros">
+            <option value="10">10</option>
+            <option value="25">25</option>
+            <option value="50">50</option>
+        </select>
+    </div>
+    <div>
+        <label for="num_registros">Registros</label>
+    </div>
     <form action="">
         <label for="">Buscar: </label>
         <input type="text" name="campo" id="campo">
@@ -34,23 +47,47 @@
 
         </tfooter>
     </table>
+    <div>
+        <div>
+            <label id="lbl-total"></label>
+        </div>
+        <div id="nav-paginacion"></div>
+    </div>
     <script>
-        getData()
-        document.getElementById("campo").addEventListener("keyup", getData)
+        let paginaActual = 1;
 
-        function getData(){
+        getData(paginaActual)
+        document.getElementById("campo").addEventListener("keyup", function(){
+            getData(1)
+        })
+        document.getElementById("num_registros").addEventListener("change", function(){
+            getData(paginaActual)
+        }, false)
+
+        function getData(pagina){
             let input = document.getElementById("campo").value
+            let num_registros = document.getElementById("num_registros").value
             let content = document.getElementById("content")
+
+            if(pagina != null){
+                paginaActual = pagina
+            }
+
             let url = "laod.php"
             let formaData = new FormData()
             formaData.append('campo', input)
+            formaData.append('registros', num_registros)
+            formaData.append('pagina', pagina)
              
             fetch(url, {
                 method: "POST",
                 body: formaData
             }).then(response => response.json())
             .then(data => {
-                content.innerHTML = data
+                content.innerHTML = data.data
+                document.getElementById("lbl-total").innerHTML = 'Mostrando' +data.totalFiltro +
+                ' de ' + data.totalRegistros + ' registros'
+                document.getElementById("nav-paginacion").innerHTML = data.paginacion
             }).catch(err => console.log(err))
         }
     </script>
